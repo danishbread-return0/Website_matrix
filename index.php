eb483ba264b7a6ac3c9e44298519ff1621e6b634
DanishBreadreturn0 <1221303139@student.mmu.edu.my>
2025-08-18 11:49:36 +0800

    Initialize commit

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/index.css">
+    
+</head>
+<body>
+    <header class="head1">
+        <a href="nav&button.php">
+            <button type="button" class="img-btn">
+                    <img src="images /HSE.png" alt="logo">
+            </button>
+         </a>
+
+
+        <nav class="navbar">
+            <ul>
+            <li>
+                <a href="index.php">Home</a>
+            </li>
+
+            <li>
+                <a href="Services.php">Services</a>
+            </li>
+
+            <li>
+                <a href="Training.php">Training</a>
+            </li>
+
+            <li>
+                <a href="Product.php">Product</a>
+            </li>
+
+            <li>
+                <a href="Location.php">Location</a>
+            </li>
+
+            <li>
+                <a href="Contact.php">Contact us</a>
+            </li>
+
+            <li>
+                <a href="Exam.php">Exam Schedule</a>
+            </li>
+            </ul>
+        </nav>
+    </header>
+    <br><br><br><br><br><br>
+    <section class="container">
+        <div class="slider-wrapper">
+            <div class="slider">
+                <img id="slide1" src="images/WebTemplate().png">
+                <img id="slide1" src="images/WebTemplate1.png">
+                <img id="slide3" src="images/WebTemplate2.png">
+                <img id="slide4" src="images/WebTemplate3.png">
+            </div>
+                <div class="slider-nav">
+                    <a data-slide="0"></a>
+                    <a data-slide="1"></a>
+                    <a data-slide="2"></a>
+                    <a data-slide="3"></a>
+                </div>
+        </div>
+
+    </section>
+    <script src="mainslide.js"></script>
+    <div>
+        <head>
+            <h1>Services Provided</h1>
+        </head>
+        <p>
+            We provide certified trainers and industry-leading consultants dedicated to improving workplace safety and ensuring DOSH & DOE compliance. Our qualified team conducts a wide range of specialized assessments, including mobile audiometric testing, noise and chemical risk assessments, local exhaust ventilation checks, ergonomic evaluations, and the control of industrial major accident hazards, helping organizations create safer and healthier work environments
+        </p>
+    </div>
+    </body>
+</html>
