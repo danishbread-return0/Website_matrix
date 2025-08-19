7224b0bd39b2c135aaa78dbafab99ecdff4b706c
Artpeii <umarqwq@gmail.com>
2025-08-19 17:08:23 +0800

    Added Training features and updated Services

--- Services.php
+++ Services.php
@@ -1,10 +1,115 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Training</title>
+    <link rel="stylesheet" href="Services.css" />
 </head>
+
+<script src="Service.js"></script>
+
 <body>
+    <section class="hero">
+        <div class="slideshow" id="slideshow">
+            <img src="img5.jpg" alt="Worker Service" class = "active"/>
+            <img src="img5.jpg" alt="Worker Service" />
+            <img src="img5.jpg" alt="Worker Service" />
+        </div>
+
+        <div class="hero-text" id="hero-text">
+            <h1>Our <span class="red">Service</span></h1>
+        </div>
+    </section>
+
+    <section class="Service-list">
+        <div class="course">
+            <div class="course-left">
+                <img src="img5.jpg" alt="Mobile Audiometric Testing" />
+            </div>     
+            <div class="vertical-line"></div>
+            <div class="course-right">
+                <h1>Mobile Audiometric Testing</h1>
+                <p>In Malaysia, the most common job-related health issue is hearing loss, caused by noise. The number of 
+                    reported cases may have gone up because more workers and employers are aware of the problem. This also 
+                    shows that job-related health issues are on the rise. Hearing loss from noise builds up over time, is 
+                    permanent, and cannot be fixed. This situation also affects the finances of workers, their families, 
+                    employers, and the government.
+                    <br><br>Workers who are exposed to loud noise, can have their hearing checked 
+                    regularly. These tests do not prevent hearing loss but are important as part of a complete health care 
+                    plan. If audiometry tests show changes in hearing, they need to be looked into to find out why and to 
+                    decide if action is needed. 
+                    <br><br>Any worker exposed to loud noise can get audiometric testing for free. 
+                    The employer must provide these tests at no cost to the workers. All tests must be done by a trained 
+                    technician at a certified testing center. We also provide doorstep service via a mobile carrier.</p>
+
+                <button class="btn clr">Click for more Inqueries</button>       
+            </div>
+        </div>
+
+        <div class="course">
+            <div class="course-left">
+                <img src="img5.jpg" alt="Mobile Audiometric Testing" />
+            </div>     
+            <div class="vertical-line"></div>
+            <div class="course-right">
+                <h1>Noise Risk Assessment (NRA)</h1>
+                <p>Noise-induced hearing loss (NIHL) is a serious work-related illness in Malaysia that costs a lot of 
+                    money. Being around loud noise also creates hidden costs for companies, like higher employee turnover
+                    and absenteeism, lower work performance, and a higher chance of accidents. People with noise-related
+                    hearing problems (ONRHD) face major challenges in their lives.
+                    <br><br>ONRHD cannot be reversed, leading to trouble communicating, problems with personal relationships,
+                    feeling isolated, and a lower quality of life. Families and friends of those affected also feel the 
+                    impact. Hearing aids can help with some hearing loss, but they cannot fully restore normal hearing.
+                    <br><br>Many people with ONRHD also experience tinnitus, which is a ringing in the ears. Because more
+                    cases of ONRHD are appearing in industries beyond manufacturing. The Department of Occupational 
+                    Safety and Health (DOSH) has created new rules in 2019 to apply safety regulations to all sectors
+                    in Malaysia.</p>
+
+                <button class="btn clr">Click for more Inqueries</button>       
+            </div>
+        </div>
+
+        <div class="course">
+            <div class="course-left">
+                <img src="img5.jpg" alt="Mobile Audiometric Testing" />
+            </div>     
+            <div class="vertical-line"></div>
+            <div class="course-right">
+                <h1>Chemical Health Risk Assessment (CHRA)</h1>
+                <p>Employers must protect workers from harmful chemicals, as required by the Occupational Safety and 
+                    Health Act 1994. To do this, they need to check all chemicals used at work to find and manage any
+                     health risks.
+                    <br><br>According to the USECHH Regulations, employers cannot use any Chemical Hazardous to 
+                    Health (CHTH) without first doing this assessment. This means they must look at any health risks
+                    from using CHTH while producing, handling, storing, transporting, or disposing of these chemicals
+                    at work. The risk assessment checks how CHTH are used and the health dangers involved. The 
+                    actions taken to keep workers safe will depend on how risky the use of these chemicals is in 
+                    specific tasks.</p>
+
+                <button class="btn clr">Click for more Inqueries</button>       
+            </div>
+        </div>
+
+        <div class="course">
+            <div class="course-left">
+                <img src="img5.jpg" alt="Mobile Audiometric Testing" />
+            </div>     
+            <div class="vertical-line"></div>
+            <div class="course-right">
+                <h1>Local Exhaust Ventilation (LEV)</h1>
+                <p>A local exhaust ventilation system is a type of equipment used in workplaces to reduce exposure to 
+                    harmful chemicals. The Occupational Safety and Health (Use and Standards Exposure to Chemicals 
+                    Hazardous to Health) Regulations 2000 set rules for how these systems should be designed, checked, 
+                    and tested. How well the system works depends on its design, how it is used, and how well it is 
+                    maintained.
+                    <br><br>The regulations define engineering control as any equipment used to reduce workers' exposure 
+                    to harmful chemicals, including local exhaust systems and water sprays.</p>
+
+                <button class="btn clr">Click for more Inqueries</button>       
+            </div>
+        </div>
+
+    </section>
 </body>
 </html>
--- Training.php
+++ Training.php
@@ -1,11 +1,223 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Training</title>
+    <link rel="stylesheet" href="Training.css" />
 </head>
+
+<script src="Training.js"></script>
+
 <body>
+    <section class="hero">
+        <div class="slideshow" id="slideshow">
+            <img src="img5.jpg" alt="Worker Training" class = "active"/>
+            <img src="img5.jpg" alt="Worker Training" />
+            <img src="img5.jpg" alt="Worker Training" />
+        </div>
+
+        <div class="hero-text" id="hero-text">
+            <h1>Our <span class="red">Training</span></h1>
+        </div>
+    </section>
+    <section class="training-list">
+        
+        
+
+        <div class="course">
+            <div class="course-left">
+                <img src="img5.jpg" alt="Steam Boiler Operator G1 G2" />
+            </div>     
+            <div class="vertical-line"></div>
+            <div class="course-right">
+                    <h1>Steam Boiler Operator (Grade 1 & Grade 2)</h1>
+                <p>Duration: 9:00 AM - 5:00 PM, 4 Days<br>Venue: Lot 180, Jalan 1A, Kg Subang Baru, 401500 Shah Alam, Selangor<br>
+                    HRD Corp Claimable Courses - Skim Bantuan Latihan Khas<br>Training Program Number: 10001471002</p>
+
+                <h2>Course Summary</h2>
+                <p>Participants will be able to understand the types of boilers, important components, and function safety
+                    features, detect abnormalities of boilers, and answer exam papers on the boiler,s issues</p>
+
+                <h2>Topic</h2>
+                <p>Introduction to Boiler, Laws Related to Steam Boilers, Steam Boiler Operations and Trouble-Shootings,
+                    Boiler Water Treatment, Combustion Fundamentals, Steam Turbines Fundamentals, Steam Boiler Maintenance, 
+                    Steam Boiler Failures, Q&A.</p>
+
+                <h2>Contact Hours</h2>
+                <p>Formal Class - 24 Hours<br>Practical - 6 Hours<br>Assessment - 1 Day</p>
+                <button class="btn clr">Training Schedule</button>
+                <button class="btn clr">Application Requirements (Grade 1)</button>
+                <button class="btn clr">Application Requirements (Grade 2)</button>
+       
+            </div>
+        </div>
+
+        <div class="course">
+            <div class="course-left">
+                <img src="img5.jpg" alt="Steam Boiler Engineer G1" />
+            </div>        
+            <div class="vertical-line"></div>
+            <div class="course-right">
+                <h1>Steam Boiler Engineer (Grade 1)</h1>
+                <p>Duration: 9:00 AM - 5:00 PM, 4 Days<br>Venue: Lot 180, Jalan 1A, Kg Subang Baru, 401500 Shah Alam, Selangor
+                <br>HRD Corp Claimable Courses - Skim Bantuan Latihan Khas<br>Training Program Number: 10001469893</p>
+
+                <h2>Course Summary</h2>
+                <p>Trainees will gain an understanding of the legislation and standards related to steam boilers, including
+                    occupational safety and health regulations codes of practice, guidelines, and roles and responsibilities of
+                    steam engineers. They will also gain an understanding of the codes and standards, conceptual design, actual
+                    design, components, fittings, and safety devices related to boiler design. Additionally, Trainees will gain a
+                    comprehensive understanding of boiler operation, including fundamental responsibilities, safety precautions,
+                    emergency procedures, maintenance practices, steam management, and efficiency considerations. Trainees will
+                    gain an understanding of boiler defects, potential root causes, materials, creep and fatigue, corrosion and
+                    control, welding mechanical failure, and non-destructive testing and inspection (NDT/NDE). Trainees will gain
+                    an understanding of machine integrity, including the concepts of fit for services and process safety management.</p>
+                    
+                <h2>Topic</h2>
+                <p>Legislation and Standards for Steam Boilers, Boiler Design, Boiler Operation, Boiler Operation, Inspection,
+                    Maintenance, and Repair of Boilers, and, Machine Integrity.</p>
+
+                <h2>Contact Hours</h2>
+                <p>Formal Class - 28 Hours</p>
+                <button class="btn clr">Training Schedule</button>
+                <button class="btn clr">Approved Letter</button>
+                <button class="btn clr">Application Requirements</button>
+
+            </div>
+        </div>
     
+        <div class="course">
+            <div class="course-left">
+                <img src="img5.jpg" alt="Steam Boiler Engineer G2" />
+            </div>
+            <div class="vertical-line"></div>
+            <div class="course-right">
+                <h1>Steam Boiler Engineer (Grade 2)</h1>
+                <p>Duration: 9:00 AM - 5:00 PM, 5 Days<br>Venue: Lot 180, Jalan 1A, Kg Subang Baru, 401500 Shah Alam, Selangor<br>
+                    HRD Corp Claimable Courses - Skim Bantuan Latihan Khas<br>Training Program Number: 1000146966</p>
+
+                <h2>Course Summary</h2>
+                <p>Trainees will be able to understand the definition, purpose, classification/comparison, fundamentals
+                    of steam, and terminology related to steam boilers.</p>
+
+                <h2>Topic</h2>
+                <p>Introduction to Steam Boilers, Legislation and Standards for Steam Boilers, Boiler Design, Boiler Installation,
+                    Testing and Commissioning, Boiler Operation, Operational Responsibilities, Storage of Idle Boiler, Introduction
+                    to Combustion, Inspection Maintenance and Repair of Boilers..</p>
+
+                <h2>Contact Hours</h2>
+                <p>Formal Class - 40 Hours</p>
+                <button class="btn clr">Training Schedule</button>
+                <button class="btn clr">Approved Letter</button>
+                <button class="btn clr">Application Requirements</button>
+            </div>
+        </div>
+
+        <div class="course">
+            <div class="course-left">
+                <img src="img5.jpg" alt="OSH-Coordinator" />
+            </div>
+            <div class="vertical-line"></div>
+            <div class="course-right">
+                <h1>OSH-Coordinator (OSH-C)</h1>
+                <p>Duration: 9:00 AM - 5:00 PM, 3 Days<br>Default Venue: Matrix HSE Resources Sdn Bhd, No. 11a, Jalan Puchong 
+                    Permai 2, Taman Puchong Permai, 47100 Puchong, Selangor<br>Other Venue: Client's Training Place/Hotel<br>
+                    HRD Corp Claimable Courses - Skim Bantuan Latihan Khas<br>Training Program Number: 1000163278</p>
+
+                <h2>Course Summary</h2>
+                <p>Trainees will be able to understand the principles, objectives, and benefits of being an OSH Co-ordinator 
+                    trained person.</p>
+
+                <h2>Topic</h2>
+                <p>Introduction to Steam Boilers, Legislation and Standards for Steam Boilers, Boiler Design, Boiler Installation,
+                    Testing and Commissioning, Boiler Operation, Operational Responsibilities, Storage of Idle Boiler, Introduction
+                    to Combustion, Inspection Maintenance and Repair of Boilers..</p>
+
+                <h2>Contact Hours</h2>
+                <p>Formal Class - 40 Hours</p>
+                <button class="btn clr">Training Schedule</button>
+                <button class="btn clr">Approved Letter</button>
+            </div>
+        </div>
+
+        <div class="course">
+            <div class="course-left">
+                <img src="img5.jpg" alt="ONE & HCP" />
+            </div>
+            <div class="vertical-line"></div>
+            <div class="course-right">
+                <h1>Occupational Noise Exposure & Hearing Conservation Program (HCP)</h1>
+                <p>Duration: 9:00 AM - 5:00 PM, 1 Days<br>Default Venue: Matrix HSE Resources Sdn Bhd, No. 11a, Jalan Puchong 
+                    Permai 2, Taman Puchong Permai, 47100 Puchong, Selangor<br>Other Venue: Client's Training Place/Hotel<br>
+                    HRD Corp Claimable Courses - Skim Bantuan Latihan Khas<br>Training Program Number: 1000158783</p>
+
+                <h2>Course Summary</h2>
+                <p>The Trainee will be able to understand the principles of the Hearing Loss Prevention Program, Noise Regulation, 
+                    Fundamentals of Noise, Noise Control, Hearing Mechanism, effect of Noise, and Audiometry Procedure</p>
+
+                <h2>Topic</h2>
+                <p>Principle of Hearing Loss Prevention Program, Fundamentals of Noise, Hearing Mechanism, Noise Control Measure, 
+                    Hearing Protection Devices, Audiometry Procedure, and Interpretation.</p>
+
+                <h2>Contact Hours</h2>
+                <p>Formal Class - 7 Hours 15 Minutes</p>
+                <button class="btn clr">Training Schedule</button>
+            </div>
+        </div>
+
+        <div class="course">
+            <div class="course-left">
+                <img src="img5.jpg" alt="First Aid" />
+            </div>
+            <div class="vertical-line"></div>
+            <div class="course-right">
+                <h1>Basic Occupational First Aid and CPR</h1>
+                <p>Duration: 9:00 AM - 5:00 PM, 2 Days<br>Default Venue: Matrix HSE Resources Sdn Bhd, No. 11a, Jalan Puchong 
+                    Permai 2, Taman Puchong Permai, 47100 Puchong, Selangor<br>Other Venue: Client's Training Place/Hotel<br>
+                    HRD Corp Claimable Courses - Skim Bantuan Latihan Khas<br>Training Program Number: 1000162375</p>
+
+                <h2>Course Summary</h2>
+                <p>Trainees will learn basic knowledge, as well as skills in rendering First Aid and CPR to enable them to deal
+                    with common injuries at the workplace and life-saving skills of CPR.</p>
+
+                <h2>Topic</h2>
+                <p>Introduction to First Aid, Wound and Bleeding, Shock, Musculoskeletal Injuries, Head & Spine Injuries, Burn 
+                    Injuries, Basic Life Support (Cardiopulmonary Resuscitation, CPR), Patient Transport, and Assessment.</p>
+
+                <h2>Contact Hours</h2>
+                <p>Formal Class - 9 Hours<br>Practical - 3 hours</p>
+                <button class="btn clr">Training Schedule</button>
+            </div>
+        </div>
+
+        <div class="course">
+            <div class="course-left">
+                <img src="img5.jpg" alt="Chemical Management" />
+            </div>
+            <div class="vertical-line"></div>
+            <div class="course-right">
+                <h1>Chemical Management</h1>
+                <p>Duration: 9:00 AM - 5:00 PM, 1 Days<br>Default Venue: Matrix HSE Resources Sdn Bhd, No. 11a, Jalan Puchong 
+                    Permai 2, Taman Puchong Permai, 47100 Puchong, Selangor<br>Other Venue: Client's Training Place/Hotel<br>
+                    HRD Corp Claimable Courses - Skim Bantuan Latihan Khas<br>Training Program Number: 1000160411</p>
+
+                <h2>Course Summary</h2>
+                <p>Trainees will be able to understand the principles of the Chemical Management Program, the USECHH 2000 
+                    Regulations, and PPE Management System. They will also learn to complete the Chemical Register.</p>
+
+                <h2>Topic</h2>
+                <p>Introduction to Chemical Safety Management, Identifying the Difference Between Hazard & Risk, Classification
+                    of Chemicals, Hazards and Properties, Safe Work System, Safety Data Sheet, Introduction to Chemical Registration, 
+                    Guidelines on how to complete the Chemical Registration, USECHH 2000 Regulations, Chemical Monitoring, 
+                    Inspection, Testing and Examination of Local Exhaust Ventilation (LEV), Spillage Prevention and Secondary 
+                    Containment, Chemical and PPE Management System, Training Requirements.</p>
+
+                <h2>Contact Hours</h2>
+                <p>Formal Class - 8 Hours 30 Minutes<br>Practical - 5 hours</p>
+                <button class="btn clr">Training Schedule</button>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
