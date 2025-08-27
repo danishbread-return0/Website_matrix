1267c153fab36904406b1264bd8c23c4d322e617
Artpeii <umarqwq@gmail.com>
2025-08-27 09:44:35 +0800

    27/8 changes

---
+++ Steam-Operator-Schedule.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+        <meta charset = "UTF-8" />
+        <meta name = "viewport" content = "width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="Steam-Operator-Schedule.css"/>
+    </head>
+    <body>
+        <header class="background-img-container"> 
+        <!-- Background image will be applied here via CSS -->
+        </header>
+        <main class="schedule-elements-box">
+            <section class="schedule-elements-inner">
+                <header class="schedule-main-headline">
+                    <h1 class="center">Steam Boiler Operator</h1> 
+                </header>
+                
+            </section>
+        </main>
+    </body>
+</html>
--- Training.php
+++ Training.php
@@ -46,7 +46,7 @@
 
                 <h2>Contact Hours</h2>
                 <p>Formal Class - 24 Hours<br>Practical - 6 Hours<br>Assessment - 1 Day</p>
-                <button class="btn clr">Training Schedule</button>
+                <button class="btn clr"data-page="">Training Schedule</button>
                 <button class="btn clr">Application Requirements (Grade 1)</button>
                 <button class="btn clr">Application Requirements (Grade 2)</button>
        
@@ -219,5 +219,6 @@
             </div>
         </div>
     </section>
+    <script>src="Training.js"</script>
 </body>
 </html>
