c2fcc714fa1f44afbf118c5a9edb4f7ad15b9bb3
Artpeii <umarqwq@gmail.com>
2025-08-28 10:54:56 +0800

    finish html

---
+++ OSH-C-Schedule.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+        <meta charset = "UTF-8" />
+        <meta name = "viewport" content = "width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="OSH-C-Schedule.css"/>
+    </head>
+    <body>
+        <header class="background-img-container"> 
+            
+        </header>
+
+        <section class="schedule-elements-box">
+            <header class="schedule-main-headline">
+                <h1 class="center">OSH-Coordinator (OSH-C)<span class="red"> Course</span></h1> 
+            </header>
+        </section>
+        <!-- schedule container -->
+        <section class="schedule-title-container">
+                <div class="schedule-title"></div>
+        </section>
+
+        <div class="in-between-title-box">
+            <div class="title-box-border"></div>
+        </div>
+
+        <!-- schedule box for first line -->
+        <main class="schedule-box-wrapper">           
+            <section class="schedule-box-container">
+                <div class="schedule-box red">
+                    <h2>September 2025</h2>
+                    <p>Date Training : <b>24, 25 & 26 September 2025 </b>
+                    <br>Closing Date  : <b>20 September 2025</b></p>
+                    <p>Register for OSH-Coordinator (OSH-C) Course</p>
+                    <button class="red-button clr">
+                        ENROLL NOW
+                    </button>
+                </div>
+                <div class="schedule-box grey">
+                    <h2>October 2025</h2>
+                    <p>Date Training : <b>13, 14 & 15 October 2025 </b>
+                    <br>Closing Date  : <b>10 October 2025</b></p>
+                    <p>Register for OSH-Coordinator (OSH-C) Course</p>
+                    <button class="grey-button clr" >
+                        ENROLL NOW
+                    </button>
+                </div>
+                <div class="schedule-box red">
+                    <h2>November 2025</h2>
+                    <p>Date Training : <b>17, 18 & 19 November 2025 </b>
+                    <br>Closing Date  : <b>14 November 2025</b></p>
+                    <p>Register for OSH-Coordinator (OSH-C) Course</p>
+                    <button class="red-button clr">
+                        ENROLL NOW
+                    </button>
+                </div>
+            </section>
+
+            <div class="in-between-box-container"></div>
+            <!-- this is the gap between above and below boxes-->
+            <section class="schedule-box-container">
+            <!-- schedule box for below -->
+                <div class="schedule-box grey">
+                    <h2>December 2025</h2>
+                    <p>Date Training : <b>15, 16 & 17 December 2025</b>
+                    <br>Closing Date  : <b>12 December 2025</b></p>
+                    <p>Register for OSH-Coordinator (OSH-C) Course</p>
+                    <button class="grey-button clr">
+                        ENROLL NOW
+                    </button>
+                </div>
+            </section>
+
+            <div class="in-between-box-container"></div>
+
+        </main>
+    </body>
+</html>
